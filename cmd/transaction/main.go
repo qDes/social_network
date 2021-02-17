033a8e3cc3e86115bf4257c4e7fb5c085947797a
Andrei <andrei.kozlov.m@gmail.com>
2021-02-17 22:30:50 +0300

    WIP

--- cmd/transaction/main.go
+++ cmd/transaction/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"social_network/internal/config"
+)
+
 func main() {
-	$END$
+	svc := config.GetSvc()
+	svc.DB.Ping()
+	query := `INSERT INTO test (id) VALUES (?);`
+
+	tx, err := svc.DB.Begin()
+	if err != nil {
+		panic(err)
+	}
+	txStmt, err := tx.Prepare(query)
+	if err != nil {
+		panic(err)
+	}
+	for i := 32701; ; i++ {
+		//fmt.Println(i)
+		_, err = txStmt.Exec(i)
+		if err != nil {
+			fmt.Println("last id", i)
+			panic(i)
+		}
+		if i % 100 == 0 {
+			tx.Commit()
+			tx, err = svc.DB.Begin()
+			fmt.Println(i)
+			txStmt, err = tx.Prepare(query)
+		}
+	}
 }
