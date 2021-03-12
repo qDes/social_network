5ad5450dc47352c6b1c3233fee88d92291329944
Andrei <andrei.kozlov.m@gmail.com>
2021-03-12 11:49:06 +0300

    WIP

--- cmd/tarantool/main.go
+++ cmd/tarantool/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
-
-	"github.com/tarantool/go-tarantool"
+	"os"
 )
 
 func main() {
+	/*
 	conn, err := tarantool.Connect("127.0.0.1:3301", tarantool.Opts{
 		User: "admin",
 		Pass: "admin",
@@ -22,4 +24,24 @@ func main() {
 	fmt.Println(resp.Data)
 
 	defer conn.Close()
+	 */
+	csvfile, err := os.Open("fakedata/fake_users.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+	r := csv.NewReader(csvfile)
+	//r := csv.NewReader(bufio.NewReader(csvfile))
+
+	// Iterate through the records
+	for i := 1;; i++ {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(i, record[0], record[1], record[2], record[3])
+	}
 }
