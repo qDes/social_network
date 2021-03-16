1f82d045dab4e189f89ad3c76a0c63913d04fd4b
Andrei <andrei.kozlov.m@gmail.com>
2021-03-16 11:45:10 +0300

    wip

--- cmd/tarantool/main.go
+++ cmd/tarantool/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
-	"reflect"
-	"social_network/internal/model"
+	"os"
 
 	"github.com/tarantool/go-tarantool"
 )
@@ -20,46 +21,45 @@ func main() {
 	if err != nil {
 		log.Fatalf("Connection refused")
 	}
-
-	resp, err := conn.Call("name_search", []interface{}{"Bobby", "Cha"})
-	//fmt.Println(resp.Data)
-
-	var res []model.User
-	for _, i := range resp.Data {
-
-		v := reflect.ValueOf(i)
-		res = append(res, model.User{ID: int64(v.Index(0).Interface().(uint64)),
-			Username: v.Index(1).Interface().(string),
-			FirstName: v.Index(2).Interface().(string),
-			SecondName: v.Index(3).Interface().(string)})
-
-	}
-	fmt.Println(res)
 	/*
-		csvfile, err := os.Open("fakedata/fake_users.csv")
-		if err != nil {
-			log.Fatalln("Couldn't open the csv file", err)
+		resp, err := conn.Call("name_search", []interface{}{"Bobby", "Cha"})
+		//fmt.Println(resp.Data)
+
+		var res []model.User
+		for _, i := range resp.Data {
+
+			v := reflect.ValueOf(i)
+			res = append(res, model.User{ID: int64(v.Index(0).Interface().(uint64)),
+				Username: v.Index(1).Interface().(string),
+				FirstName: v.Index(2).Interface().(string),
+				SecondName: v.Index(3).Interface().(string)})
+
 		}
-		r := csv.NewReader(csvfile)
-
-		// Iterate through the records
-		for i := 1;; i++ {
-			// Read each record from csv
-			record, err := r.Read()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-			//fmt.Println(i, record[0], record[2], record[3])
-			resp, err := conn.Insert("users", []interface{}{i, record[0], record[2], record[3]})
-			if err != nil{
-				log.Fatal(err)
-			}
-			fmt.Println(resp.Data)
-		}
-
-
+		fmt.Println(res)
 	*/
+
+	csvfile, err := os.Open("fakedata/fake_users.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+	r := csv.NewReader(csvfile)
+
+	// Iterate through the records
+	for i := 1; ; i++ {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Println(i, record[0], record[2], record[3])
+		resp, err := conn.Insert("users", []interface{}{i, record[0], record[2], record[3]})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(resp.Data)
+	}
+
 }
--- tarantool/name_search.lua
+++ tarantool/name_search.lua
@@ -1,6 +1,6 @@
 function name_search(first, second)
     local ret = {}
-    for _, tuple in box.space.users.index.secondary:pairs({first, second}, {iterator='GE'}) do
+    for _, tuple in box.space.users.index.secondary:pairs({second, first}, {iterator='GE'}) do
         if (string.startswith(tuple[3], first, 1, -1) and string.startswith(tuple[4], second, 1, -1)) then
             table.insert(ret, tuple)
         end
