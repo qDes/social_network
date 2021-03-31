e8269a9a3f72ce731e110af01c38885b325881fc
Andrei <andrei.kozlov.m@gmail.com>
2021-03-31 21:03:59 +0300

    vitess demo

---
+++ sharding/vitess/tables/unsharded_keyspace_schema_file.sql
@@ -0,0 +1,14 @@
+CREATE TABLE dinosaurs (
+  id BIGINT UNSIGNED,
+  time_created_ns BIGINT UNSIGNED,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE eggs (
+  id BIGINT UNSIGNED,
+  time_created_ns BIGINT UNSIGNED,
+  species VARCHAR(255),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
